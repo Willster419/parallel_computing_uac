93dc08d8e382c22255f63692643f6ae49eac8a8b
Willster419 <willster419@outlook.com>
2020-06-06 21:43:39 -0700

    finished with getting the 2d arrays to work by manipulating the hell out of pointers

--- PAC4/p3.c
+++ PAC4/p3.c
@@ -9,7 +9,7 @@
 #define MOVING_AVERAGE_AMMOUNT 9
 
 float average(int collum, float matrix[][HEIGHT]);
-//float movingAverage(int collum, int row, float *matrix);
+float movingAverage(int collum, int row, float *matrix);
 void initMatrix(int width, int height, float matrix[width][height]);
 void initVector(int width, float *vector);
 
@@ -36,27 +36,29 @@ int main(int argc, char **argv)
   initVector(WIDTH, vectorAverage);
   
   printf("Initialize the moving average vector\n");
-  initMatrix(WIDTH, HEIGHT, matrixMove);
+  initMatrix(WIDTH - MOVING_AVERAGE_AMMOUNT, HEIGHT, matrixMove);
 
   // for each colum, get a average value
-  printf("Get averages of a row for each colum\n");
+  printf("Get averages of a row for each column\n");
   for(i = 0; i < WIDTH; i++)
   {
     vectorAverage[i] = average(i, matrix);
     printf("Average of vector %d is %f\n",i,vectorAverage[i]);
   }
-  exit(0);
 
   // for each colum for each row get the moving average
-  /*
+  printf("Get moving averages for each row for each column\n");
   for(i = 9; i < WIDTH; i++)
   {
     for(j = 0; j < HEIGHT; j++)
     {
-      matrixMove[i][j] = movingAverage(i,j,matrix);
+      // https://www.tutorialspoint.com/cprogramming/c_pointer_to_an_array.htm
+      // https://www.studytonight.com/c/pointers-with-array.php
+      matrixMove[i][j] = movingAverage(i,j, &matrix[0][0]);
+      printf("Moving average from point %d, %d is %f\n",i,j,matrixMove[i][j]);
     }
   }
-  */
+
 }
 
 float average(int collum, float matrix[][HEIGHT])
@@ -70,19 +72,25 @@ float average(int collum, float matrix[][HEIGHT])
   average_ = average_ / HEIGHT;
   return average_;
 }
-/*
-float movingAverage(int collum, int row, float **matrix)
+
+float movingAverage(int collum, int row, float *matrix)
 {
   int i;
   float average = 0.0f;
-  for(i = MOVING_AVERAGE_AMMOUNT; i < 0; i++)
+  // It is legal to use array names as constant pointers, and vice versa. Therefore, *(balance + 4) is a legitimate way of accessing the data at balance[4].
+  // a[i][j] is the same as *(*(a + i) + j)
+  // *((arr+i*n) + j)
+
+  for(i = MOVING_AVERAGE_AMMOUNT; i > 0; i--)
   {
-    average += matrix[collum-i][row];
+    //average += matrix[collum-i][row];
+    average += *((matrix+collum*WIDTH) + i);
+    //printf("MovingAverage adding value to now %f\n", average);
   }
   average = average / MOVING_AVERAGE_AMMOUNT;
   return average;
 }
-*/
+
 void initMatrix(int width, int height, float matrix[width][height])
 {
   int i, j = 0;
